3454a600557ee7402f0b4fe763c20e521b58527a
jsha21 <jsha@ais21.co.kr>
2025-03-27 12:55:07 +0900

    first commit

---
+++ lib/flutter_mcp_common_web.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+import 'package:flutter_web_plugins/flutter_web_plugins.dart';
+
+/// Web implementation of the flutter_mcp_common plugin.
+class FlutterMcpCommonWeb {
+  static void registerWith(Registrar registrar) {
+  }
+}
